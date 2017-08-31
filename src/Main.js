67933665408384a0a2ec5d8e725b8ad9fe414e9d
Kevin Zhang <kzhang@federatedwireless.com>
2017-08-30 21:13:36 -0400

    random temp text for prod testing

--- src/Main.js
+++ src/Main.js
@@ -9,17 +9,53 @@ class Main extends Component {
         <h1>hey, i'm kevin</h1>
         <ul>
           <HoverItem
-            top={0}
-            left={0}
+            top={3}
+            left={10}
+            page="about"
             text="about">
-            <p>new england based developer</p>
+            <p>kevin zhang</p>
+            <p>frontend engineer @ federated wireless</p>
+            <p className="new">
+              hey there - i'm a developer slash designer from boston, ma.
+              i'm a graduate of rensselaer polytechnic institute's class of
+              2017, though i entered with the class of 2018.
+            </p>
+            <p className="new">
+              i have an unhealthy fascination with fountain pens.
+              send help
+            </p>
           </HoverItem>
-          <li>contact</li>
-          <li>journal</li>
+          <HoverItem
+            top={3}
+            left={10}
+            page="contact"
+            text="contact">
+            <p>hello [at] zhangkev.in</p>
+            <p className="new">though i doubt anyone's gonna scrape that email...</p>
+          </HoverItem>
+          <HoverItem
+            top={3}
+            left={10}
+            page="journal"
+            text="journal">
+            <p>personal thoughts and opinions are mine and mine alone</p>
+          </HoverItem>
         </ul>
         <ul>
-          <li>github</li>
-          <li>instagram</li>
+          <HoverItem
+            top={3}
+            left={10}
+            page="github"
+            text="github">
+            <p>various codebases of questionable usefulness</p>
+          </HoverItem>
+          <HoverItem
+            top={3}
+            left={10}
+            page="instagram"
+            text="instagram">
+            <p>personal stuff for now - but maybe i'll start posting art</p>
+          </HoverItem>
         </ul>
       </div>
     );
