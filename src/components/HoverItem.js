848be83a7cd9b05c7cce7529334c91a16a7b36c3
Kevin Zhang <kzhaaang@gmail.com>
2017-09-03 23:11:51 -0400

    fix lint

--- src/components/HoverItem.js
+++ src/components/HoverItem.js
@@ -3,45 +3,45 @@ import PropTypes from 'prop-types';
 import className from 'classnames';
 
 export default class HoverItem extends Component {
-  static propTypes = {
-    top: PropTypes.number.isRequired,
-    left: PropTypes.number.isRequired,
-    page: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
-  };
+		static propTypes = {
+			top: PropTypes.number.isRequired,
+			left: PropTypes.number.isRequired,
+			page: PropTypes.string.isRequired,
+			text: PropTypes.string.isRequired
+		};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hover: false
-    };
-  }
+		constructor(props) {
+			super(props);
+			this.state = {
+				hover: false
+			};
+		}
 
-  hover = () => {
-    this.setState({hover: !this.state.hover});
-  }
+		hover = () => {
+			this.setState({hover: !this.state.hover});
+		}
 
-  render() {
-    const {
-      children,
-      top,
-      left,
-      page,
-      text
-    } = this.props;
-    return(
-      <li>
-        <span
-          onMouseEnter={this.hover}
-          onMouseLeave={this.hover}
-          style={{cursor: "pointer"}}>
-          {text}
-        </span>
-        <div className={className((this.state.hover) ? "hover" : "hide", page)}
-          style={{top: `${top}em`, left: `${left}em`}}>
-          {children}
-        </div>
-      </li>
-    )
-  }
-};
+		render() {
+			const {
+				children,
+				top,
+				left,
+				page,
+				text
+			} = this.props;
+			return(
+				<li>
+					<span
+						onMouseEnter={this.hover}
+						onMouseLeave={this.hover}
+						style={{cursor: 'pointer'}}>
+						{text}
+					</span>
+					<div className={className((this.state.hover) ? 'hover' : 'hide', page)}
+						style={{top: `${top}em`, left: `${left}em`}}>
+						{children}
+					</div>
+				</li>
+			);
+		}
+}
