a52bf44d1c3e74494637918a2d8354cfb8ef9378
chocolatemelt <kzhaaang@gmail.com>
2019-07-12 23:27:26 -0400

    add support for devtools extension

--- src/configureStore.js
+++ src/configureStore.js
@@ -5,15 +5,14 @@ import rootReducer from './reducers';
 
 export default function configureStore(preloadedState) {
   const loggerMiddleware = createLogger();
-
   const middleware = applyMiddleware(thunkMiddleware, loggerMiddleware);
+  const store = middleware(createStore)(
+    rootReducer,
+    preloadedState,
+    /* eslint-disable no-underscore-dangle */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    /* eslint-enable */
+  );
 
-  const store = middleware(createStore)(rootReducer, preloadedState);
-
-  // return createStore(
-  //   rootReducer,
-  //   preloadedState,
-  //   applyMiddleware(thunkMiddleware, loggerMiddleware),
-  // );
   return store;
 }
