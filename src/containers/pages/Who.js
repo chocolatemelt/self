7c119a1a9dccaf386ea9d52546705c4fa9f99dcd
chocolatemelt <kzhaaang@gmail.com>
2019-07-13 16:01:06 -0400

    update with resume and cv links

--- src/containers/pages/Who.js
+++ src/containers/pages/Who.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Who = () => (
   <div className="page who">
     <p>
-      sup—i'm a multi-disciplinary programmer hanging out in the greater boston area of
+      sup—i'm a multi-disciplinary developer hanging out in the greater boston area of
       massachusetts.
     </p>
     <p className="br">
-      i'm passionate about all things visual, and have been drawing on-and-off for the past 3      years. in addition, i'm something of a fountain pen connoisseur, which is about as nerdy as it
-      gets.
+      i'm passionate about all things visual, and have been drawing on-and-off for the past 3 years.
+      in addition, i'm something of a fountain pen connoisseur, which is about as nerdy as it gets.
     </p>
     <p className="br">come say hi:</p>
     <p>hello [at] kevinzha.ng</p>
+    <p>
+      <a href="public/Kevin_Zhang_Résumé.pdf">résumé</a> | <Link to="/cv">cv</Link>
+    </p>
   </div>
 );
 
