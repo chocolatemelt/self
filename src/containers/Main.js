bc6d8cc3324d20dbb9c5ef8e38f78c6c31bb981b
chocolatemelt <kzhaaang@gmail.com>
2019-07-13 01:07:23 -0400

    simple react routing

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import HoverItem from '../components/HoverItem';
+import Nav from '../components/Nav';
+
+function Index() {
+  return <h2>nothing to see but us chickens</h2>;
+}
+
+function About() {
+  return <h2>about</h2>;
+}
+
+function Projects() {
+  return <h2>projects</h2>;
+}
+
+function Drawings() {
+  return <h2>drawings</h2>;
+}
+
+function Compositions() {
+  return <h2>comps</h2>;
+}
 
 const Main = () => (
   <Router>
     <div className="main">
       <h1>
-        <a className="lead" href="/who">
+        <Link className="lead" to="/who">
           hey, i'm kevin
-        </a>
+        </Link>
       </h1>
       <ul>
-        <HoverItem link="/projects">projects</HoverItem>
-        <HoverItem link="/drawings">drawings</HoverItem>
-        <HoverItem link="/compositions">compositions</HoverItem>
+        <Nav link="/projects">projects</Nav>
+        <Nav link="/drawings">drawings</Nav>
+        <Nav link="/compositions">compositions</Nav>
       </ul>
       <ul>
-        <HoverItem href="https://github.com/chocolatemelt">github</HoverItem>
-        <HoverItem href="https://instagram.com/itsuwaru">instagram</HoverItem>
+        <Nav href link="https://github.com/chocolatemelt">
+          github
+        </Nav>
+        <Nav href link="https://instagram.com/itsuwaru">
+          instagram
+        </Nav>
       </ul>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/drawings" component={Drawings} />
+      <Route path="/compositions" component={Compositions} />
+      <Route path="/who" component={About} />
     </div>
   </Router>
 );
