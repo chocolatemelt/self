202856f52d47286e1b6e88766ff90c60c2349a36
chocolatemelt <kzhaaang@gmail.com>
2019-07-13 16:01:34 -0400

    initial cv commit

---
+++ src/containers/pages/CurriculumVitae.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CurriculumVitae = () => (
+  <div className="page cv">
+    <p>omegalol</p>
+    <p className="br">
+      <Link to="/who">back</Link>
+    </p>
+  </div>
+);
+
+export default CurriculumVitae;
