0e3f2b5a8cbda6585b417b023dfa5af7b60eb159
chocolatemelt <kzhaaang@gmail.com>
2019-07-13 18:04:16 -0400

    remove line

--- src/containers/pages/Who.js
+++ src/containers/pages/Who.js
@@ -8,8 +8,8 @@ const Who = () => (
       massachusetts.
     </p>
     <p className="br">
-      i'm passionate about all things visual, and have been drawing on-and-off for the past 3 years.
-      in addition, i'm something of a fountain pen connoisseur, which is about as nerdy as it gets.
+      i'm passionate about all things visual. in addition, i'm something of a fountain pen
+      connoisseur, which is about as nerdy as it gets.
     </p>
     <p className="br">come say hi:</p>
     <p>hello [at] kevinzha.ng</p>
