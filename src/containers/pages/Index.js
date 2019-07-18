bc9539c9a828deb7cc8c54e23cd969038bf39a42
chocolatemelt <kzhaaang@gmail.com>
2019-07-18 01:48:56 -0400

    add akali corner

--- src/containers/pages/Index.js
+++ src/containers/pages/Index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Index = () => <div className="page" />;
+const Index = () => (
+  <div className="page">
+    <img className="hi" src="/raw/i/profile.png" alt="hi" />
+  </div>
+);
 
 export default Index;
