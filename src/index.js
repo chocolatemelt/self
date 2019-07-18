082cf898302df9542dfc826880e1daf8f8249d5a
chocolatemelt <kzhaaang@gmail.com>
2019-07-18 01:50:38 -0400

    adjust store management

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Root from './containers/Root';
-import configureStore from './configureStore';
+import store from './configureStore';
 import registerServiceWorker from './registerServiceWorker';
 import './css/reset.css';
 import './css/ricty.css';
 import './css/index.css';
-
-const store = configureStore();
 
 ReactDOM.render(<Root store={store} />, document.getElementById('main'));
 
