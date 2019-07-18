705ec9f05c32e800f21f9187f8f6191d4459ae87
chocolatemelt <kzhaaang@gmail.com>
2019-07-17 23:46:23 -0400

    add assorted drawings

---
+++ src/containers/pages/drawings/DrawingsAssorted.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Case from '../../../components/Case';
+
+const DrawingsAssorted = () => (
+  <div className="page">
+    <Case>
+      <img src="/raw/d/misc/elisanne.png" alt="elisanne portrait" />
+      <img src="/raw/d/misc/akali.png" alt="kda akali profile sketch" />
+      <img src="/raw/d/misc/lyn.png" alt="lyn" />
+      <img src="/raw/d/misc/freeform.png" alt="stretching girl" />
+    </Case>
+  </div>
+);
+
+export default DrawingsAssorted;
