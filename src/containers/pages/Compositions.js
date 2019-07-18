83030215a03accce2ba1ce25e97d054ea71bfdae
chocolatemelt <kzhaaang@gmail.com>
2019-07-17 21:16:37 -0400

    delete original pages

--- src/containers/pages/Compositions.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Compositions = () => <div className="page">designs - coming soon</div>;
-
-export default Compositions;
--- src/containers/pages/Drawings.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Drawings = () => <div className="page">drawings - coming soon</div>;
-
-export default Drawings;
